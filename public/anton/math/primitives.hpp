1022567ab6a299fba6af60114d3e4507432d32a9
Piotr Kocia <p.kocia@wp.pl>
2021-11-11 13:37:18 +0100

    added extent3

--- public/anton/math/primitives.hpp
+++ public/anton/math/primitives.hpp
@@ -8,6 +8,24 @@ namespace anton::math {
         Vec3 direction;
     };
     
+    struct Extent3 {
+        Vec3 min;
+        Vec3 max;
+    };
+
+    // outer_bounds
+    // Forms an extent that encompasses both e1 and e2, i.e. the outer bounds.
+    //
+    // Parameters:
+    // e1, e2 - extents to calculate the outer bounds of.
+    //
+    // Returns: 
+    // An extent encompassing both e1 and e2.
+    //
+    [[nodiscard]] inline Extent3 outer_bounds(Extent3 const& e1, Extent3 const& e2) {
+        return {min(e1.min, e2.min), max(e1.max, e2.max)};
+    }
+
     struct OBB {
         Vec3 center;
         Vec3 local_x;
